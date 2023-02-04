28ac9c103483b06a6309aed8a26566cf1d43e1c5
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-02-04 14:39:13 +0100

    Drop files from .gitignore

--- archive/main02.cc
+++ archive/main02.cc
@@ -1,22 +1,38 @@
-#include "rtweekend.h"
-
 #include "color.h"
-#include "hittable_list.h"
-#include "sphere.h"
+#include "ray.h"
+#include "vec3.h"
 
 #include <iostream>
 
-color ray_color(const ray& r, const hittable& world)
+bool hit_sphere(const point3& center, double radius, const ray& r)
 {
-	hit_record rec;
+	vec3 oc = r.origin() - center;
 
-	if (world.hit(r, 0, infinity, rec))
-		return .5 * (rec.normal + color(1, 1, 1));
+	auto a = dot(r.direction(), r.direction());
+	auto b = 2.0 * dot(oc, r.direction());
+	auto c = dot(oc, oc) - radius*radius;
+
+	auto discriminant = b*b - 4*a*c;
+
+	auto outlineThickness = .1;
+	bool isHittingOutline = 0 < discriminant && discriminant < outlineThickness;
+
+	bool isHittingSphere = 0 < discriminant;
+
+	return isHittingSphere;
+}
+
+color ray_color(const ray& r)
+{
+	if (hit_sphere(point3(0, 0, -1), 0.5, r))
+	{
+		return color(1, 0, 0);
+	}
 
 	vec3 unit_direction = unit_vector(r.direction());
-	auto t = .5 * (unit_direction.y() + 1.0);
+	auto t = 0.5 * (unit_direction.y() + 1.0);
 
-	return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 0.5);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main()
@@ -24,14 +40,8 @@ int main()
 	// image
 
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 1000;
+	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-
-	// world
-	
-	hittable_list world;
-	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-	world.add(make_shared<sphere>(point3(0, -100.5, 1), 100));
 
 	// camera
 	
@@ -59,7 +69,7 @@ int main()
 
 			ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
 
-			color pixel_color = ray_color(r, world);
+			color pixel_color = ray_color(r);
 			write_color(std::cout, pixel_color);
 		}
 	}
--- archive/main03.cc
+++ archive/main03.cc
@@ -1,22 +1,44 @@
-#include "rtweekend.h"
-
 #include "color.h"
-#include "hittable_list.h"
-#include "sphere.h"
+#include "ray.h"
+#include "vec3.h"
 
 #include <iostream>
 
-color ray_color(const ray& r, const hittable& world)
+double hit_sphere(const point3& center, double radius, const ray& r)
 {
-	hit_record rec;
+	vec3 oc = r.origin() - center;
 
-	if (world.hit(r, 0, infinity, rec))
-		return .5 * (rec.normal + color(1, 1, 1));
+	auto a = r.direction().length_squared();
+	auto half_b = dot(oc, r.direction());
+	auto c = oc.length_squared() - radius*radius;
+
+	auto discriminant = half_b*half_b - a*c;
+
+	if (discriminant < 0)
+	{
+		return -1.0;
+	}
+	else
+	{
+		return (-half_b - sqrt(discriminant)) / a;
+	}
+}
+
+color ray_color(const ray& r)
+{
+	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+
+	if (t > 0.0)
+	{
+		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+
+		return 0.5 * color(N.x()+1, N.y()+1, N.z()+1);
+	}
 
 	vec3 unit_direction = unit_vector(r.direction());
-	auto t = .5 * (unit_direction.y() + 1.0);
+	t = 0.5 * (unit_direction.y() + 1.0);
 
-	return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 0.5);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main()
@@ -24,14 +46,8 @@ int main()
 	// image
 
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 1000;
+	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-
-	// world
-	
-	hittable_list world;
-	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-	world.add(make_shared<sphere>(point3(0, -100.5, 1), 100));
 
 	// camera
 	
@@ -59,7 +75,7 @@ int main()
 
 			ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
 
-			color pixel_color = ray_color(r, world);
+			color pixel_color = ray_color(r);
 			write_color(std::cout, pixel_color);
 		}
 	}
